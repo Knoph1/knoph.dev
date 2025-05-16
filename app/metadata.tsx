184c06a1a190fe935d99337e92c495c3e93d926d
Knoph <knopholuoch@yahoo.com>
2025-05-16 12:50:51 +0300

    Add files via upload

--- app/metadata.tsx
+++ app/metadata.tsx
@@ -3,5 +3,5 @@ import type { Metadata } from "next"
 export const metadata: Metadata = {
   title: "Projects | Knoph O. Ayieko Portfolio",
   description: "Explore the projects built by Knoph O. Ayieko, showcasing skills in web development and design",
-  keywords: ["projects", "portfolio", "web development", "case studies"],
+  keywords: ["knoph.dev", "projects", "portfolio", "developer", "portfolio", "full stack", "web development", "software developer", "web developer", "IT specialist", "IT consultant", "virtual assistant", , "case studies"],
 }
